256d4b11c1380bf2189a29a378c4c16be319a690
safdar211ali <safdar211@hotmail.com>
2023-12-10 20:58:55 +0500

    updated

---
+++ amafhh/amafhh/doctype/roll_to_roll_conversion/roll_to_roll_conversion.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Tech Ventures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Roll To Roll Conversion', {
+	// refresh: function(frm) {
+
+	// }
+});
