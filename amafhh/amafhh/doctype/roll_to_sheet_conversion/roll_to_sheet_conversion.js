202ced8bce918183987731a50e5b839119b0b2eb
safdar211ali <safdar211@hotmail.com>
2023-12-14 22:39:30 +0500

    updated

--- amafhh/amafhh/doctype/roll_to_sheet_conversion/roll_to_sheet_conversion.js
+++ amafhh/amafhh/doctype/roll_to_sheet_conversion/roll_to_sheet_conversion.js
@@ -9,33 +9,33 @@ frappe.ui.form.on('Roll To Sheet Conversion', {
 
 frappe.ui.form.on('Roll To Sheet Conversion Source', {
 
-    // sr_no: function (frm, cdt, cdn) {
-    //     var row = locals[cdt][cdn];
-    //     if (row.sr_no) {
-    //         frappe.call({
-    //             method: 'amafhh.amafhh.doctype.utils.get_sr_no.get_sr_no',
-    //
-    //             args: {
-    //                 sr_no: row.sr_no
-    //             },
-    //             callback: function (response) {
-    //                 if (response.message) {
-    //                     frappe.model.set_value(cdt, cdn, 'item_code', response.message.item_code);
-    //                     frappe.model.set_value(cdt, cdn, 'rate', response.message.purchase_rate);
-    //                     frappe.model.set_value(cdt, cdn, 'amount', response.message.purchase_amount);
-    //                     frappe.model.set_value(cdt, cdn, 'weightkg', response.message.weight_balance);
-    //                     frappe.model.set_value(cdt, cdn, 'width', response.message.width);
-    //                     frappe.model.set_value(cdt, cdn, 'gsm', response.message.gsm);
-    //
-    //                     frappe.model.set_value(cdt, cdn, 'amount', row.rate * row.weightkg);
-    //                 } else {
-    //                     frappe.msgprint(__('Record not found for SR No: {0}', [row.sr_no]));
-    //                     frappe.model.set_value(cdt, cdn, 'item_code', '');
-    //                 }
-    //             }
-    //         });
-    //     }
-    // },
+    sr_no: function (frm, cdt, cdn) {
+        var row = locals[cdt][cdn];
+        if (row.sr_no) {
+            frappe.call({
+                method: 'amafhh.amafhh.doctype.utils.get_sr_no.get_sr_no',
+
+                args: {
+                    sr_no: row.sr_no
+                },
+                callback: function (response) {
+                    if (response.message) {
+                        frappe.model.set_value(cdt, cdn, 'item_code', response.message.item_code);
+                        frappe.model.set_value(cdt, cdn, 'rate', response.message.purchase_rate);
+                        frappe.model.set_value(cdt, cdn, 'amount', response.message.purchase_amount);
+                        frappe.model.set_value(cdt, cdn, 'weightkg', response.message.weight_balance);
+                        frappe.model.set_value(cdt, cdn, 'width', response.message.width);
+                        frappe.model.set_value(cdt, cdn, 'gsm', response.message.gsm);
+
+                        frappe.model.set_value(cdt, cdn, 'amount', row.rate * row.weightkg);
+                    } else {
+                        frappe.msgprint(__('Record not found for SR No: {0}', [row.sr_no]));
+                        frappe.model.set_value(cdt, cdn, 'item_code', '');
+                    }
+                }
+            });
+        }
+    },
     weightkg: function (frm, cdt, cdn) {
         var row = locals[cdt][cdn];
         frappe.model.set_value(cdt, cdn, 'amount', row.rate * row.weightkg);
@@ -57,26 +57,26 @@ frappe.ui.form.on('Roll To Sheet Conversion Source', {
 });
 frappe.ui.form.on('Roll To Sheet Conversion Target', {
 
-    sr_no: function (frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        if (row.sr_no) {
-            frappe.call({
-                method: 'amafhh.amafhh.doctype.utils.get_sr_no.get_sr_no',
-
-                args: {
-                    sr_no: row.sr_no
-                },
-                callback: function (response) {
-                    if (response.message) {
-                        frappe.model.set_value(cdt, cdn, 'item_code', response.message.item_code);
-                    } else {
-                        frappe.msgprint(__('Record not found for SR No: {0}', [row.sr_no]));
-                        frappe.model.set_value(cdt, cdn, 'item_code', '');
-                    }
-                }
-            });
-        }
-    },
+    // sr_no: function (frm, cdt, cdn) {
+    //     var row = locals[cdt][cdn];
+    //     if (row.sr_no) {
+    //         frappe.call({
+    //             method: 'amafhh.amafhh.doctype.utils.get_sr_no.get_sr_no',
+    //
+    //             args: {
+    //                 sr_no: row.sr_no
+    //             },
+    //             callback: function (response) {
+    //                 if (response.message) {
+    //                     frappe.model.set_value(cdt, cdn, 'item_code', response.message.item_code);
+    //                 } else {
+    //                     frappe.msgprint(__('Record not found for SR No: {0}', [row.sr_no]));
+    //                     frappe.model.set_value(cdt, cdn, 'item_code', '');
+    //                 }
+    //             }
+    //         });
+    //     }
+    // },
     roll_to_sheet_conversion_target_add: function (frm, cdt, cdn) {
         frappe.model.set_value(cdt, cdn, 'gsm', frm.fields_dict['roll_to_sheet_conversion_source'].grid.data[0].gsm);
         frappe.model.set_value(cdt, cdn, 'rate', frm.fields_dict['roll_to_sheet_conversion_source'].grid.data[0].rate);
