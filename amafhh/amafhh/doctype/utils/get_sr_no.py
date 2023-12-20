d7c0b6c1a468736890264efe87d353e3fbf977a0
safdar211ali <safdar211@hotmail.com>
2023-12-20 19:55:01 +0500

    updated

--- amafhh/amafhh/doctype/utils/get_sr_no.py
+++ amafhh/amafhh/doctype/utils/get_sr_no.py
@@ -4,6 +4,19 @@ import frappe
 @frappe.whitelist()
 def get_sr_no(sr_no):
     data = {}
+    balance = 0
+    stock = frappe.db.sql(
+        """
+        select 
+           SUM(CASE WHEN `tabStock Ledger Entry`.actual_qty < 0 THEN `tabStock Ledger Entry`.actual_qty ELSE 0 END) as balance_qty
+        from `tabStock Entry`, `tabStock Ledger Entry`
+        where `tabStock Entry`.name = `tabStock Ledger Entry`.voucher_no AND `tabStock Entry`.docstatus = 1 AND `tabStock Ledger Entry`.is_cancelled = 0
+            AND `tabStock Entry`.sr_no =  %s  
+        """, (sr_no),
+        as_dict=1
+    )
+    if stock[0].balance_qty and stock[0].balance_qty<0:
+        balance = stock[0].balance_qty
     sr_no_doc = frappe.get_doc("SR NO", sr_no)
     if sr_no_doc:
         data.update(
@@ -11,7 +24,7 @@ def get_sr_no(sr_no):
                 "item_code": sr_no_doc.item_code,
                 "rate": sr_no_doc.rate,
                 "amount": sr_no_doc.amount,
-                "weight_balance": sr_no_doc.weight_balance,
+                "weight_balance":float(sr_no_doc.weight_total) + balance,
             }
         )
         item = frappe.get_doc("Item", sr_no_doc.item_code)
