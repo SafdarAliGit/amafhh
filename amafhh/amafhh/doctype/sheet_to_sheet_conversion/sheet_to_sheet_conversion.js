17fcbff71cb38a6c9433785dec34b5f4ecd6228d
SafdarAliGit <safdar211@gmail.com>
2024-01-30 22:02:56 +0500

    update

--- amafhh/amafhh/doctype/sheet_to_sheet_conversion/sheet_to_sheet_conversion.js
+++ amafhh/amafhh/doctype/sheet_to_sheet_conversion/sheet_to_sheet_conversion.js
@@ -98,7 +98,7 @@ function calculate_source_target_weight_total(frm) {
     var weight_target = 0;
     $.each(frm.doc.sheet_to_sheet_conversion_items || [], function (i, d) {
 
-        weight_source += flt(d.stock_weight_source);
+        weight_source += flt(d.weight_source);
         weight_target += flt(d.weight_target);
     });
     frm.set_value('source_weight', weight_source);
