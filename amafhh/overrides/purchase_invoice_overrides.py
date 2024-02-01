5c3225e65d5be5a9f6be871eefccf9fe28efd2eb
SafdarAliGit <safdar211@gmail.com>
2024-02-01 20:56:16 +0500

    updated

--- amafhh/overrides/purchase_invoice_overrides.py
+++ amafhh/overrides/purchase_invoice_overrides.py
@@ -15,7 +15,7 @@ class PurchaseInvoiceOverrides(PurchaseInvoice):
                 batch.amount = item.amount
                 batch.item_group = item.item_group
                 batch.ref_no = self.name
-                batch.import_file = item.import_file
+                batch.import_file = self.import_file
                 batch.gsm = item.gsm
                 batch.ref_type = "Purchase Invoice"
                 try:
@@ -29,7 +29,7 @@ class PurchaseInvoiceOverrides(PurchaseInvoice):
                 product_item.amount = item.amount
                 product_item.item_group = item.item_group
                 product_item.ref_no = self.name
-                product_item.import_file = item.import_file
+                product_item.import_file = self.import_file
                 product_item.gsm = item.gsm
                 product_item.ref_type = "Purchase Invoice"
                 product_item.qty = item.qty
