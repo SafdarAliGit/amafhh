10940266b71b0f8055e12244eb858b51a054993b
SafdarAliGit <safdar211@gmail.com>
2024-02-04 21:40:46 +0500

    update

--- amafhh/amafhh/doctype/utils/get_by_item_code.py
+++ amafhh/amafhh/doctype/utils/get_by_item_code.py
@@ -4,6 +4,20 @@ import frappe
 @frappe.whitelist()
 def get_by_item_code(item_code):
     data = {}
+    stock = frappe.get_all(
+        "Stock Ledger Entry",
+        filters={"item_code": item_code},
+        fields=["qty_after_transaction"],
+        order_by="name desc",
+        limit=1
+    )
+    if stock:
+        data.update(
+            {
+                "weight_balance": float(stock[0].qty_after_transaction) if stock[0].qty_after_transaction else 0
+            }
+        )
+
     item = frappe.get_doc("Item", item_code)
     if item:
         data.update(
@@ -14,7 +28,6 @@ def get_by_item_code(item_code):
                 "item_code": item.item_code,
                 "rate": item.rate,
                 "amount": item.amount,
-                "weight_balance": float(item.qty)  if item.qty else 0,
                 "import_file": item.import_file
             }
         )
