9323341b2fdc0082b4835910c7e07ec3ba86498b
SafdarAliGit <safdar211@gmail.com>
2024-04-18 17:28:38 +0500

    updated

--- amafhh/amafhh/doctype/import_file/import_file.js
+++ amafhh/amafhh/doctype/import_file/import_file.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Import File', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+        var import_file = frm.doc.name;
+        if (import_file) {
+            me.frm.call({
+                method: "amafhh.amafhh.doctype.utils.update_avg_rate.update_avg_rate",
+                args: {
+                    import_file: import_file
+                },
+                callback: function (r, rt) {
+                    if (r.message) {
+                        frm.set_value("total_purchase_qty", r.message.total_purchase_qty);
+                        frm.set_value("avg_purchase_rate", r.message.avg_purchase_rate);
+                        frm.set_value("total_purchase_amount", r.message.total_purchase_amount);
+                        frm.set_value("total_lc_amount", r.message.total_lc_amount);
+                        frm.set_value("total_cost", r.message.total_cost);
+                        frm.set_value("avg_rate_with_lc", r.message.avg_rate_with_lc);
+                    }
+                }
+            });
+        }
+	}
 });
--- amafhh/amafhh/doctype/utils/update_avg_rate.py
+++ amafhh/amafhh/doctype/utils/update_avg_rate.py
@@ -3,6 +3,7 @@ import frappe
 
 @frappe.whitelist()
 def update_avg_rate(**args):
+    # -----------START-------
     import_file = args.get('import_file')
     pi = frappe.qb.DocType("Purchase Invoice")
     pii = frappe.qb.DocType("Purchase Invoice Item")
@@ -40,36 +41,30 @@ def update_avg_rate(**args):
 
     lcv_parent_query_result = lcv_parent_query.run(as_dict=True)
 
-    # Now you can return both results
     # -------------Purchase Invoice----------------
-    total_qty = 0
+    total_purchase_qty = 0
     total_rate = 0
-    total_amount = 0
+    total_purchase_amount = 0
     for purchase in pi_parent_query_result:
-        total_qty += purchase.qty
+        total_purchase_qty += purchase.qty
         total_rate += purchase.rate
-        total_amount += purchase.amount
-    if len(pi_parent_query_result) != 0:
-        avg_rate = total_rate / len(pi_parent_query_result)
-    else:
-        # Handle the case where len(purchase_result) is zero
-        avg_rate = 0  # or any other appropriate value
-    # -------------Landed Cost Voucher----------------
-    total_lc_amount = 0
-    for lcr in lcv_parent_query_result:
-        total_lc_amount += lcr.amount
+        total_purchase_amount += purchase.amount
 
-    total_cost = total_amount + total_lc_amount
-    if total_qty != 0:
-        avg_rate_with_lc = total_cost / total_qty
-    else:
-        avg_rate = 0
+    avg_purchase_rate = total_rate / len(pi_parent_query_result) if pi_parent_query_result else 0
+
+    # -------------Landed Cost Voucher----------------
+    total_lc_amount = sum(lcr.amount for lcr in lcv_parent_query_result)
+
+    total_cost = total_purchase_amount + total_lc_amount
+    avg_rate_with_lc = total_cost / total_purchase_qty if total_purchase_qty != 0 else 0
+
     return {
-        'total_qty': round(total_qty, 2),
-        'avg_rate': round(avg_rate, 2),
-        'total_amount': round(total_amount, 2),
+        'total_purchase_qty': round(total_purchase_qty, 2),
+        'avg_purchase_rate': round(avg_purchase_rate, 2),
+        'total_purchase_amount': round(total_purchase_amount, 2),
         'total_lc_amount': round(total_lc_amount, 2),
         'total_cost': round(total_cost, 2),
         'avg_rate_with_lc': round(avg_rate_with_lc, 2)
-
     }
+
+    # ----------END----------
